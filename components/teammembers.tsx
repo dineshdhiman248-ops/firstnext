deb17400344932d037d3516856181d015f8605e7
Dinesh Kumar <dineshdhiman248@gmail.com>
2026-05-27 13:56:29 +0530

    teammembers updated

--- components/teammembers.tsx
+++ components/teammembers.tsx
@@ -1,32 +1,32 @@
-     
-function TeamMembers( ) {
-return(  
-<div className="container mx-auto px-4 md:px-6 py-8 md:py-16"> 
-<div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
-<div className="rounded-2xl border border-gray-300 bg-white/5 p-5 backdrop-blur-sm text-center">
-<img src="/images/about.png" alt="Member 1" className="w-16 h-16 rounded-full mb-4 mx-auto" />
-<h4 className="mb-2 text-xl font-semibold text-black-400">Member 1</h4>
-<p className="text-sm">Deliver innovative and impactful solutions for businesses worldwide.</p></div>
+function TeamMembers({ members }: any) {
+  return (
+    <div className="container mx-auto px-4 md:px-6 py-8 md:py-16">
+      <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
 
+        {members.map((member: any) => (
+          <div
+            key={member.id}
+            className="rounded-2xl border border-gray-300 bg-white/5 p-5 backdrop-blur-sm text-center"
+          >
+            <img
+              src={member.image}
+              alt={member.name}
+              className="w-16 h-16 rounded-full mb-4 mx-auto"
+            />
 
-<div className="rounded-2xl border border-gray-300 bg-white/5 p-5 backdrop-blur-sm text-center">
-<img src="/images/about.png" alt="Member 1" className="w-16 h-16 rounded-full mb-4 mx-auto" />
-<h4 className="mb-2 text-xl font-semibold text-black-400">Member 2</h4>
-<p className="text-black text-sm">Deliver innovative and impactful solutions for businesses worldwide.</p></div>
+            <h4 className="mb-2 text-xl font-semibold text-black">
+              {member.name}
+            </h4>
 
-<div className="rounded-2xl border border-gray-300 bg-white/5 p-5 backdrop-blur-sm text-center">
-<img src="/images/about.png" alt="Member 1" className="w-16 h-16 rounded-full mb-4 mx-auto" />
-<h4 className="mb-2 text-xl font-semibold text-black-400">Member 3</h4>
-<p className="text-black text-sm">Deliver innovative and impactful solutions for businesses worldwide.</p></div>
+            <p className="text-sm text-black">
+              {member.description}
+            </p>
+          </div>
+        ))}
 
-<div className="rounded-2xl border border-gray-300 bg-white/5 p-5 backdrop-blur-sm text-center   ">
-<img src="/images/about.png" alt="Member 1" className="w-16 h-16 rounded-full mb-4 mx-auto" />
-<h4 className="mb-2 text-xl font-semibold text-black-400">Member 4</h4>
-<p className="text-black text-sm">Deliver innovative and impactful solutions for businesses worldwide.</p></div>
-
-
-</div>
-</div>
-    )
+      </div>
+    </div>
+  );
 }
+
 export default TeamMembers;
